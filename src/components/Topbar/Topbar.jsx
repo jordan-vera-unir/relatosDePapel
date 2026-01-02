471c69fbcf38d72d933a77ec267f3dcc8befa37a
Jordan Vera <jordan.vera667@comunidadunir.net>
2026-01-02 12:24:30 -0500

    se agrega title a boton de carrito de compras para accesibilidad

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -71,6 +71,7 @@ function Topbar() {
             strokeLinecap="round"
             strokeLinejoin="round"
           >
+            <title>Boton de carrito de compras</title>
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
             <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
