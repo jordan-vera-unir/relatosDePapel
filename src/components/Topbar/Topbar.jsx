816c69a4f927f7330cf2b5bd25441e2ef8981683
Jordan Vera <jordan.vera667@comunidadunir.net>
2026-01-02 12:10:49 -0500

    Se agrega title a  el logo de relatos de papel, para accesibilidad

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -21,6 +21,7 @@ function Topbar() {
           strokeLinecap="round"
           strokeLinejoin="round"
         >
+          <title>Logo de relatos de papel es un libro</title>
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M19 4v16h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12z" />
           <path d="M19 16h-12a2 2 0 0 0 -2 2" />
