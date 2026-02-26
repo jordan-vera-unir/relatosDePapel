cc9058f30f26ef6ad8ffeede941bd67384437db4
Jordan Vera <jordan.vera667@comunidadunir.net>
2026-02-26 08:47:44 -0500

    Se realiza ajuste para consumir el servicio de libros

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,11 +1,14 @@
-import { createContext, useState } from "react";
-import booksData from "../data/books.json";
+import { createContext, useState, useEffect } from "react"; // 1. Agregamos useEffect
+//import booksData from "../data/books.json";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const GlobalContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
-  const [books] = useState(booksData);
+
+  //const [books] = useState(booksData);
+  const [books, setBooks] = useState([]);
+
   const [esModalAbierto, setEsModalAbierto] = useState(false);
   const mostrarModal = () => setEsModalAbierto(!esModalAbierto);
   const [busqueda, setBusqueda] = useState("");
@@ -18,6 +21,21 @@ export const GlobalProvider = ({ children }) => {
       return [];
     }
   });
+
+  // 3. Implementamos la llamada al servicio
+  useEffect(() => {
+    const obtenerLibros = async () => {
+      try {
+        const respuesta = await fetch("https://spring-cloud-gateway-production-7bac.up.railway.app/ms-books-catalogue/book");
+        const datos = await respuesta.json();
+        setBooks(datos.books);
+      } catch (error) {
+        console.error("Error cargando los libros:", error);
+      }
+    };
+
+    obtenerLibros();
+  }, []); // El array vacío asegura que solo se ejecute una vez al cargar
 
   return (
     <GlobalContext.Provider
